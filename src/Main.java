7752a336ca2b4ba0603c8e0873d482550be24ed9
Mykola <murza.na00@gmail.com>
2023-05-27 19:23:40 +0300

    lesson 15

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,43 @@
-import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException {
-        Class<?> userClass = Class.forName("models.User");
-        Constructor<?> constructor = userClass.getConstructor(String.class, int.class, String.class);
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+        Main dummyObj = new Main();
+        Method[] methods = Main.class.getDeclaredMethods();
+        List<Method> sortedMethods = Arrays.stream(methods)
+                .filter(method -> method.isAnnotationPresent(CallSequence.class))
+                .sorted(Comparator.comparingInt(method -> method.getAnnotation(CallSequence.class).position()))
+                .toList();
 
-        System.out.println(constructor.getName());
-        System.out.println(constructor.getDeclaringClass());
-        System.out.println(constructor.getAnnotatedReturnType().getType().getTypeName());
-        System.out.println(constructor.getParameterCount());
+        for (Method method : sortedMethods) {
+            if (Modifier.isStatic(method.getModifiers())) { // Якщо метод статичний - викликаємо без об'єкта.
+                method.invoke(null);
+            } else { // Якщо метод не статичний - передаємо потрібний об'єкт
+                method.invoke(dummyObj);
+            }
+        }
+    }
+
+    @CallSequence(position = Integer.MIN_VALUE)
+    public static void start() {
+        System.out.println("Start");
+    }
+
+    @CallSequence(position = 1)
+    public static void doWork() {
+        System.out.println("Do something...");
+    }
+
+    //... КУПА МЕТОДІВ!
+
+    @CallSequence(position = Integer.MAX_VALUE)
+    public static void finish() {
+        System.out.println("Finish");
     }
 }
+
--- src/models/Writing.java
+++
@@ -1,5 +0,0 @@
-package models;
-
-public interface Writing {
-    void write(String text);
-}
