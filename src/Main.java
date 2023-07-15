c156eba4c454320bd99158d6c6042fea3e0fa482
Mykola <murza.na00@gmail.com>
2023-07-15 16:51:04 +0300

    Prepare to record

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,5 @@
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-
 public class Main {
-    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
-        Main dummyObj = new Main();
-        Method[] methods = Main.class.getDeclaredMethods();
-        List<Method> sortedMethods = Arrays.stream(methods)
-                .filter(method -> method.isAnnotationPresent(CallSequence.class))
-                .sorted(Comparator.comparingInt(method -> method.getAnnotation(CallSequence.class).position()))
-                .toList();
+    public static void main(String[] args) {
 
-        for (Method method : sortedMethods) {
-            if (Modifier.isStatic(method.getModifiers())) { // Якщо метод статичний - викликаємо без об'єкта.
-                method.invoke(null);
-            } else { // Якщо метод не статичний - передаємо потрібний об'єкт
-                method.invoke(dummyObj);
-            }
-        }
-    }
-
-    @CallSequence(position = Integer.MIN_VALUE)
-    public static void start() {
-        System.out.println("Start");
-    }
-
-    @CallSequence(position = 1)
-    public static void doWork() {
-        System.out.println("Do something...");
-    }
-
-    //... КУПА МЕТОДІВ!
-
-    @CallSequence(position = Integer.MAX_VALUE)
-    public static void finish() {
-        System.out.println("Finish");
     }
 }
-
