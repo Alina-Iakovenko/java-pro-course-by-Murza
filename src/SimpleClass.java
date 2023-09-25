b8e5879ef0fad5744108d05470a6eda2f59ea758
MykolaMurza <murza.na00@gmail.com>
2023-09-25 15:35:58 +0300

    prep lesson-22

--- src/SimpleClass.java
+++
@@ -1,9 +0,0 @@
-import java.io.Serial;
-import java.io.Serializable;
-
-public class SimpleClass implements Serializable {
-    @Serial
-    private static final long serialVersionUID = 1L;
-    private String simpleField;
-}
-
