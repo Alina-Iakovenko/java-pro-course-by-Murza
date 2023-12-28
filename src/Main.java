b169c56b07869098b528a9418ead2ca4eeee2d82
MykolaMurza <murza.na00@gmail.com>
2023-12-28 14:04:01 +0200

    lesson 23

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.io.Console;
+
 public class Main {
     public static void main(String[] args) {
+        Console console = System.console();
+        String username =
+                console.readLine("Enter username: ");
+        char[] password =
+                console.readPassword("Hello, %s! Enter password: ", username);
 
+        System.out.printf("%s : %s", username, new String(password));
     }
 }
